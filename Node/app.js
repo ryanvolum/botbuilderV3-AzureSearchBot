fe0924e1948766e93c5141b4218a8b24bdd684ac
Christopher Harrison <chrhar@microsoft.com>
2017-04-17 16:27:47 -0700

    Began updates to rework dialogs

--- Node/app.js
+++ Node/app.js
@@ -1,35 +1,44 @@
- require('./config.js')();
+require('./config.js')();
 require('./connectorSetup.js')();
 require('./searchHelpers.js')();
-require('./dialogs/results.js')(); 
+require('./dialogs/results.js')();
 require('./dialogs/musicianExplorer.js')();
 require('./dialogs/musicianSearch.js')();
 
 
-var request = require('request');
+const request = require('request');
+const restify = require('restify');
+const builder = require('botbuilder');
 
-// Entry point of the bot
-bot.dialog('/', [
-    function (session) {
-        session.replaceDialog('/promptButtons');
-    }
-]);
+//If testing via the emulator, no need for appId and appPassword. If publishing, enter appId and appPassword here 
+const connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID ? process.env.MICROSOFT_APP_ID : '',
+    appPassword: process.env.MICROSOFT_APP_PASSWORD ? process.env.MICROSOFT_APP_PASSWORD : '',
+    gzipData: true
+});
 
-bot.dialog('/promptButtons', [
-    function (session) {
-        var choices = ["Musician Explorer", "Musician Search"]
-        builder.Prompts.choice(session, "How would you like to explore the classical music bot?", choices);
+// create the bot
+const bot = new builder.UniversalBot(connector, (session) => session.replaceDialog('promptButtons'));
+
+bot.dialog('promptButtons', [
+    (session) => {
+        const choices = ['Musician Explorer', 'Musician Search'];
+        builder.Prompts.choice(session, 
+            'How would you like to explore the classical music bot?',
+            choices, 
+            { listStyle: builder.ListStyle.button }
+        );
     },
-    function (session, results) {
+    (session, results) => {
         if (results.response) {
-            var selection = results.response.entity;
+            const selection = results.response.entity;
             // route to corresponding dialogs
             switch (selection) {
                 case "Musician Explorer":
-                    session.replaceDialog('/musicianExplorer');
+                    session.replaceDialog('musicianExplorer');
                     break;
                 case "Musician Search":
-                    session.replaceDialog('/musicianSearch');
+                    session.replaceDialog('musicianSearch');
                     break;
                 default:
                     session.reset('/');
@@ -38,6 +47,20 @@ bot.dialog('/promptButtons', [
         }
     }
 ]);
+
+bot.dialog('musicianExplorer', require('./dialogs/musicianExplorer.js'));
+
+// reset stuck dialogs in case of versioning
+bot.use(builder.Middleware.dialogVersion({ version: 0.2, resetCommand: /^reset/i }));
+
+
+// Setup Restify Server
+const server = restify.createServer();
+server.post('/api/messages', connector.listen());
+
+server.listen(process.env.port || 3978, () => {
+    console.log('%s listening to %s', server.name, server.url);
+});
 
 
 

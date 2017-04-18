10dc31ad0c47342d329d54be4fccc8c7e1ca700c
Christopher Harrison <chrhar@microsoft.com>
2017-04-18 15:10:13 -0700

    Added searchQuery function

--- Node/searchHelpers.js
+++ Node/searchHelpers.js
@@ -1,20 +1,8 @@
-module.exports = function () {
-    global.request = require('request');
-
-    global.searchQueryStringBuilder = function (query) {
-        return queryString + query;
-    }
-}
-
-
 const request = require('request');
-const rootQueryString = ``
+const rootQueryString = `https://${process.env.AZURE_SEARCH_NAME}.search.windows.net/indexes/${process.env.AZURE_INDEX_NAME}/docs?api-key=${process.env.AZURE_SEARCH_KEY}&api-version=2016-09-01`
 
 module.exports = {
-    
     facetQuery: (facet, callback) => {
-        checkConfig();
-
         const queryString = `${rootQueryString}&facet=${facet}`;
         request(queryString, (error, response, body) => {
             if(error) {
@@ -31,21 +19,37 @@ module.exports = {
         });
     },
     filterQuery: (filterName, filterValue, callback) => {
-        const queryString = `$filter=${filterName} eq '${filterValue}'`;
+        const queryString = `${rootQueryString}&$filter=${filterName} eq '${filterValue}'`;
 
         request(queryString, (error, response, body) => {
             if(error) {
                 callback(error, null);
-            } else if(!result || !result['value'] || !result['value'][0]) {
-                // no items found
-                callback(null, null);
             } else {
-                callback(null, result['value']);
+                const result = JSON.parse(body);
+                if(result.value && result.value.length > 0) {
+                    callback(null, result.value);
+                } else {
+                    callback(null, null);
+                }
+            }
+        });
+    },
+    searchQuery: (keyword, callback) => {
+        const queryString = `${rootQueryString}&search=${keyword}`;
+        request(queryString, (error, response, body) => {
+            if(error) {
+                callback(error, null);
+            } else {
+                const result = JSON.parse(body);
+                if(result && result.value) {
+                    callback(null, result.value);
+                } else {
+                    callback(null, null);
+                }
             }
         });
     },
     performSearchQuery: (queryString, callback) => {
-
         request(queryString, (error, response, body) => {
             if (!error && response && response.statusCode === 200) {
                 const result = JSON.parse(body);

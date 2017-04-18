67d4634640a91cbebece4c1b558a702996076885
Christopher Harrison <chrhar@microsoft.com>
2017-04-18 12:15:55 -0700

    Reworked root dialog structure

--- Node/app.js
+++ Node/app.js
@@ -23,50 +23,35 @@ const connector = new builder.ChatConnector({
 });
 
 // create the bot
-const bot = new builder.UniversalBot(connector, (session) => session.replaceDialog('promptButtons'));
-bot.dialog(dialogs.musicianExplorer.id, dialogs.musicianExplorer.dialog);
-bot.dialog(dialogs.musicianSearch.id, dialogs.musicianSearch.dialog);
+const bot = new builder.UniversalBot(connector, (session) => {
+    const message = new builder.Message(session);
+    message.text = 'How would you like to search?';
+    message.attachments([
+        builder.CardAction.imBack(
+            session, dialogs.musicianExplorer.title
+        ),
+        builder.CardAction.imBack(
+            session, dialogs.musicianSearch.title
+        ),
+    ]);
+});
 
-bot.dialog('promptButtons', [
-    (session) => {
-        const choices = ['Musician Explorer', 'Musician Search'];
-        builder.Prompts.choice(session, 
-            'How would you like to explore the classical music bot?',
-            choices, 
-            { listStyle: builder.ListStyle.button }
-        );
-    },
-    (session, results) => {
-        if (results.response) {
-            const selection = results.response.entity;
-            // route to corresponding dialogs
-            switch (selection) {
-                case "Musician Explorer":
-                    session.replaceDialog('musicianExplorer');
-                    break;
-                case "Musician Search":
-                    session.replaceDialog('musicianSearch');
-                    break;
-                default:
-                    session.reset('/');
-                    break;
-            }
-        }
-    }
-]);
+// register the two dialogs
+// musicianExplorer will provide a facet or category based search
+bot.dialog(dialogs.musicianExplorer.id, dialogs.musicianExplorer.dialog)
+    .triggerAction({ matches: new RegExp(dialogs.musicianExplorer.title, 'i') });
 
+// musicianSearch will provide a classic search
+bot.dialog(dialogs.musicianSearch.id, dialogs.musicianSearch.dialog)
+    .triggerAction({ matches: new RegExp(dialogs.musicianSearch.title, 'i') });
 
 // reset stuck dialogs in case of versioning
 bot.use(builder.Middleware.dialogVersion({ version: 0.2, resetCommand: /^reset/i }));
-
 
 // Setup Restify Server
 const server = restify.createServer();
 server.post('/api/messages', connector.listen());
 
-server.listen(process.env.port || 3978, () => {
+server.listen(process.env.PORT, () => {
     console.log('%s listening to %s', server.name, server.url);
 });
-
-
-
